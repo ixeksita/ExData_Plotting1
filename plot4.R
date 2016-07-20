ee676280ea1517087bcab29b4a398956398039c8
Tania Sanchez <tania.sanchezmonroy@gmail.com>
2016-07-20 13:43:28 +0100

    Update plot4.R

--- plot4.R
+++ plot4.R
@@ -9,10 +9,6 @@ energy$Date<-as.Date(energy$Date,"%d/%m/%Y")
 #subsetting the dataset
 energy1=subset(energy, Date>="2007-02-01" & Date<="2007-02-02")
 
-#converting the time to Time class
-energy1$Time<-as.POSIXlt(energy1$Time,"GMT","%H:%M:%S")
-energy1$Time<-strftime(energy1$Time,"%H:%M:%S", "GMT")
-
 #adding a datetime column (this will be used for the plots)
 energy1<-mutate(energy1,datetime=as.POSIXct(paste(Date,Time), "%Y-%m-%d %H:%M%:%S"))
 
